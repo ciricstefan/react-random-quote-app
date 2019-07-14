7499224ae72acdb67e6b6cb07dfd814390c2d1d7
ciricstefan <ciric.stefan95@gmail.com>
2019-07-14 22:07:03 +0200

    create fetch api req for random quote and its author

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -3,6 +3,23 @@ import Quote from "./Quote";
 import Footer from "./Footer";
 
 export default class Wrapper extends Component {
+  state = {
+    quote: "",
+    author: ""
+  };
+
+  componentDidMount() {
+    fetch("http://quotes.stormconsultancy.co.uk/random.json")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          quote: data.quote,
+          author: data.author
+        });
+      })
+      .catch(console.log);
+  }
+
   render() {
     return (
       <div id="quote-box">
