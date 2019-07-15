19ad1e5314e4fd4c4e7fcccaf06fbfae83638eff
ciricstefan <ciric.stefan95@gmail.com>
2019-07-15 21:52:42 +0200

    create and style Footer component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,36 +2,38 @@ import React from "react";
 
 export default function Quote({ quote, author, getRandomQuote }) {
   return (
-    <div>
-      <p id="text">
-        <span>"</span>
-        {quote}
-        <span>"</span>
-      </p>
-      <p id="author">
-        <span>Author:</span> {author}
-      </p>
+    <div id="quote-box">
       <div>
-        <button
-          id="new-quote"
-          onClick={() => {
-            getRandomQuote();
-          }}
-        >
-          Get A New Quote
-        </button>
-        <a
-          href={
-            "https://twitter.com/intent/tweet?text=" +
-            quote +
-            " - " +
-            author +
-            "&hashtags=randomquoteday"
-          }
-          id="tweet-quote"
-        >
-          <button>Tweet This Awesome Quote</button>
-        </a>
+        <p id="text">
+          <span>"</span>
+          {quote}
+          <span>"</span>
+        </p>
+        <p id="author">
+          <span>Author:</span> {author}
+        </p>
+        <div>
+          <button
+            id="new-quote"
+            onClick={() => {
+              getRandomQuote();
+            }}
+          >
+            Get A New Quote
+          </button>
+          <a
+            href={
+              "https://twitter.com/intent/tweet?text=" +
+              quote +
+              " - " +
+              author +
+              "&hashtags=randomquoteday"
+            }
+            id="tweet-quote"
+          >
+            <button>Tweet This Awesome Quote</button>
+          </a>
+        </div>
       </div>
     </div>
   );
