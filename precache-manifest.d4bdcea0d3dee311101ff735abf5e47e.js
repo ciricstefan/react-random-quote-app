0f8de4f90fce44f97fb0e444f23adf999e5cd0a4
ciricstefan <ciric.stefan95@gmail.com>
2019-07-15 21:57:30 +0200

    Updates

---
+++ precache-manifest.d4bdcea0d3dee311101ff735abf5e47e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0aa25d7c2c59805dbd1963e80b2348",
+    "url": "/react-random-quote-app/index.html"
+  },
+  {
+    "revision": "ca1a6179634a65d0e76f",
+    "url": "/react-random-quote-app/static/css/main.b120881b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-random-quote-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ca1a6179634a65d0e76f",
+    "url": "/react-random-quote-app/static/js/main.869e94f4.chunk.js"
+  },
+  {
+    "revision": "faa1cafa67a7debfcafc",
+    "url": "/react-random-quote-app/static/js/runtime~main.81489eda.js"
+  }
+]);
